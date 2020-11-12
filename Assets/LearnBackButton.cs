c7d54d789708362ca68b5b19c61cefdeea036b36
EunjiBahk <eup7017@thi.de>
2020-11-12 18:55:06 +0100

    project folder commit
    base project

---
+++ Assets/LearnBackButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LearnBackButton : MonoBehaviour
+{
+    public void GoBack() {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/MainInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainInterface : MonoBehaviour
+{
+    public void PlayLearn() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+}
