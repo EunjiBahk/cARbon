c1b4113146c10ccad037f8d2fab0449e68ff26d2
EunjiBahk <eup7017@thi.de>
2020-11-16 16:08:00 +0100

    modify game scene and add comments on source code

--- Assets/Script/QuizManager.cs
+++ Assets/Script/QuizManager.cs
@@ -25,110 +25,132 @@ public class QuizManager : MonoBehaviour
     int totlaQuestions = 0;
     public int score;
 
+    // Start game
     private void Start()
     {
+        // Count how many questions we have
         totlaQuestions = QnA.Count;
+        // Disable Gameover panel 
         Gopanel.SetActive(false);
+        // Generate question
         generateQuestion();
     }
+    
+    // Generate question
+    void generateQuestion()
+    {
+        // If we have questions left go into here 
+        if(QnA.Count > 0)
+        {
+            // Make a random question from the question list
+            currentQuestion = Random.Range(0, QnA.Count);
+            // Set question text
+            QuestionTxt.text = QnA[currentQuestion].Question;
+            
+            // Disable all child object of image target
+            for (int i = 0; i < theTrackable.gameObject.transform.childCount; i++)
+            {
+                theTrackable.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            
+            // Make a parent to 3d model as image target
+            QnA[currentQuestion].augmentationObject.transform.parent = theTrackable.transform;
+            // Enable 3d model
+            QnA[currentQuestion].augmentationObject.SetActive(true);
+            
+            // Change the location of 3d model a little bit
+            if(QnA[currentQuestion].Question=="CH4")
+            {
+                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(0.03f,0.05f,0.06f);
+            } 
+            else if(QnA[currentQuestion].Question=="C3H8")
+            {
+                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(-0.03f,0.11f,-0.06f);
+            } 
+            else if(QnA[currentQuestion].Question=="C3H6")
+            {
+                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(-0.03f,0.08f,-0.04f);
+            }
+            
+            QnA[currentQuestion].augmentationObject.transform.localRotation = Quaternion.identity;
+            
+            // Set answers
+            SetAnswers();            
+        }
+        else // If we don't have any question
+        {
+            // Game over and show result
+            GameOver();
+        }
+    }
 
+    // Retry game
     public void retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    // Enable Gameover panel and Show result
     public void GameOver()
     {
+        // Disable Quiz panel 
         Quizpanel.SetActive(false);
+        // Enable Gameover panel 
         Gopanel.SetActive(true);
+        // Set score text e.g. 1/3, 2/3, 3/3..
         ScoreTxt.text = score + "/" + totlaQuestions;
         if (score>=3)
         {
+            // If you solve all questions, you get great messege
             ResultTxt.text = "GREAT!!";
         } else if (score>=2 && score<3)
         {
+            // If you solve most questions, you get good messege
             ResultTxt.text = "GOOD!";
         } else
         {
+            // If you solve few questions, you get bad messege
             ResultTxt.text = "BAD..";
         }       
         
     }
 
+    // If you select correct answer
     public void correct()
     {
+        // You can get additional score
         score += 1;
+        // Remove current question from the question list
         QnA.RemoveAt(currentQuestion);
+        // Genereate next question
         generateQuestion();
     }
 
+    // If you select wrong answer
     public void wrong()
     {
+        // Remove current question from the question list
         QnA.RemoveAt(currentQuestion);
+        // Genereate next question
         generateQuestion();
     }
 
+    // Set Answer
     void SetAnswers()
     {
         for (int i=0; i< options.Length; i++)
         {
+            // Set value of all answer as false first (initiate)
             options[i].GetComponent<AnswerScript>().isCorrect = false;
+            // Set text from the answer option
             options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
 
+            // If specific answer is correct, set value as true 
             if(QnA[currentQuestion].CorrectAnswer == i+1)
             {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
-            //Debug.Log(options[i].GetComponent<AnswerScript>().isCorrect);
         }
     }
 
-    void generateQuestion()
-    {
-        if(QnA.Count > 0)
-        {
-            //Debug.Log(QnA.Count); // 2
-            currentQuestion = Random.Range(0, QnA.Count);
-            //Debug.Log(currentQuestion); // 0 or 1
-            QuestionTxt.text = QnA[currentQuestion].Question;
-            
-            //image target
-            GameObject augmentationModel = theTrackable.gameObject;
-
-            for (int i = 0; i < augmentationModel.transform.childCount; i++)
-            {
-                Transform child = augmentationModel.transform.GetChild(i);
-                child.gameObject.SetActive(false);
-            }
-
-            //3d model
-            QnA[currentQuestion].augmentationObject.transform.parent = theTrackable.transform;
-            QnA[currentQuestion].augmentationObject.SetActive(true);
-            
-            if(QnA[currentQuestion].Question=="CH4")
-            {
-                Debug.Log("CH4"); 
-                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(0.03f,0.05f,0.06f);
-            } 
-            else if(QnA[currentQuestion].Question=="C3H8")
-            {
-                Debug.Log("C3H8"); 
-                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(-0.03f,0.11f,-0.06f);
-            } 
-            else if(QnA[currentQuestion].Question=="C3H6")
-            {
-                Debug.Log("C3H6");
-                //QnA[currentQuestion].augmentationObject.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-                QnA[currentQuestion].augmentationObject.transform.localPosition = new Vector3(-0.03f,0.08f,-0.04f);
-            }
-            
-            QnA[currentQuestion].augmentationObject.transform.localRotation = Quaternion.identity;
-            SetAnswers();            
-        }
-        else
-        {
-            Debug.Log("Out of Questions");
-            GameOver();
-        }
-    }
 }
